0d6a8467fb7afe60ddca7418f348fa72fbe3ec9b
Guilherme Serpa Batista <serpa419@gmail.com>
2023-01-19 21:44:49 -0300

    teste up

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -34,7 +34,6 @@ export default async (req, res) => {
       folder: "demo",
       use_filename: true,
       resource_type: 'raw',
-      public_id: "doc",
       filename_override: data?.files?.inputFile.originalFilename
     });
     try {
